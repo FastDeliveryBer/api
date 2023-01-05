f3a47b1de8329d3df570893d73655ec2b2065098
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2023-01-05 23:11:47 +0100

    Mise en place d'une route d'exemple et amélioration disposition

--- index.js
+++ index.js
@@ -2,37 +2,26 @@ import express from 'express'
 import dotenv from 'dotenv'
 import swaggerUI from 'swagger-ui-express'
 import docs from './docs/index.js'
-import morgan from 'morgan'
-import { createWriteStream } from 'fs'
+import configlog from './log/log.js'
+import deliverer from './server/deliverer/deliverer.routes.js'
 
 dotenv.config()
 
 const app = express()
-const router = express.Router()
 const HOST = process.env.HOST || 'http://localhost'
 const PORT = parseInt(process.env.PORT || '3000')
 
+/**
+ * Middlewares
+ */
+app.use(configlog)
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(docs))
 
-const accessLogStream = createWriteStream('./log/access.log', {
-  flags: 'a',
-})
-app.use(
-  morgan(
-    ':date[clf] :remote-addr - :user-agent[family] :user-agent[major].:user-agent[minor] - :method :url[full] :status :response-time ms - :res[content-length]',
-    { stream: accessLogStream }
-  )
-)
-
-router.get('/test', (req, res) => {
-  res.status(200).send('test')
-})
-router.get('/test2', (req, res) => {
-  res.status(200).send('test')
-})
-
-app.use(router)
+/**
+ * Route initialization
+ */
+app.use('/deliverer', deliverer)
 
 app.listen(PORT, async () => {
-  console.log(`Application started on URL ${HOST}:${PORT} 🎉`)
+  console.log('\x1b[43m%s\x1b[0m', `API listening at ${HOST}:${PORT}`)
 })
--- server/deliverer/deliverer.routes.js
+++ server/deliverer/deliverer.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express'
-import DeliveryCtrl from './deliverer.controller'
+import DeliveryCtrl from './deliverer.controller.js'
 
 const router = Router()
-
-router.get('/tournee', _) //Récupérer les tournées d'un livreur
-router.get('/places', DeliveryCtrl.msgUser) //Récupérer les points de livraion sur une carte
-router.post('/photos', _) //Ajouter des photosà une livraison
-router.post('/update/langage', _) //Changer la langue enregistré pour l'application
+router.get('/tournee', DeliveryCtrl.fnc) //Récupérer les tournées d'un livreur
+router.get('/places', DeliveryCtrl.fnc) //Récupérer les points de livraion sur une carte
+router.post('/photos', DeliveryCtrl.fnc) //Ajouter des photosà une livraison
+router.post('/update/langage', DeliveryCtrl.fnc) //Changer la langue enregistré pour l'application
 
 export default router
