8b50fcee8ed26d40a1152df107b6b9b63c59650e
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2023-01-31 10:06:20 +0100

    Nettoyage fichiers inutilisé

--- server/auth/deliverer.controller.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const { Request, Response } = express
-export default class DeliveryCtrl {
-  static fnc = (req = Request, res = Response) => {
-    res.status(200).send('test')
-  }
-}
